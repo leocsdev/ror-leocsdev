4ac52695d8bd1e9ccf8d66a0247fb82d73af7742
Leo <leocsdev@gmail.com>
2024-04-09 22:54:04 +0800

    Created helper to render svg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def render_svg(name, options = {})
+    options[:title] ||= name.underscore.humanize
+    options[:aria] = true
+    options[:nocomment] = true
+    options[:class] = options.fetch(:styles, "")
+
+    filename = "#{name}.svg"
+    inline_svg_tag(filename, options)
+  end
 end
