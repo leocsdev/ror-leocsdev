c749863349042d8d256dbdddd98754907f2e24cd
Leo Santillan <38428303+leocsdev@users.noreply.github.com>
2024-04-09 23:19:31 +0800

    Add static pages and homepage hero section (#2)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def render_svg(name, options = {})
+    options[:title] ||= name.underscore.humanize
+    options[:aria] = true
+    options[:nocomment] = true
+    options[:class] = options.fetch(:class, "")
+
+    filename = "#{name}.svg"
+    inline_svg_tag(filename, options)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "static_pages#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  root "homepage#index"
+  get "/about", to: "static_pages#about"
 end
