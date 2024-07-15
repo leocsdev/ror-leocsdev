96b8ae405d36106bb1e9fa8ea3618f54b86b440f
leo-narra <141992134+leo-narra@users.noreply.github.com>
2024-07-15 19:57:43 +0800

    rails g model BlogPost title image description body:text

---
+++ db/migrate/20240715115645_create_blog_posts.rb
@@ -0,0 +1,12 @@
+class CreateBlogPosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :blog_posts do |t|
+      t.string :title
+      t.string :image
+      t.string :description
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
