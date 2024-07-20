d8bd8fd8045a6056ceccee906edeb19d829dc6d7
leo-narra <141992134+leo-narra@users.noreply.github.com>
2024-07-20 10:28:03 +0800

    Install annotate gem.

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: blog_posts
+#
+#  id          :bigint           not null, primary key
+#  body        :text
+#  description :string
+#  image       :string
+#  published   :boolean          default(FALSE)
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class BlogPost < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true, length: { minimum: 5 }
+  validates :body, presence: true, length: { minimum: 5 }
 end
--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: blog_posts
+#
+#  id          :bigint           not null, primary key
+#  body        :text
+#  description :string
+#  image       :string
+#  published   :boolean          default(FALSE)
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
