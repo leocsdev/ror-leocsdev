3e4e56d871d4381ca5b2244b86e817bc91c8e02e
leo-narra <141992134+leo-narra@users.noreply.github.com>
2024-07-21 22:42:28 +0800

    Refactor @blog_post instance

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,11 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @blog_posts = BlogPost.published.latest
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
   end
 
   def new
@@ -22,12 +23,9 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
   end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
-
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
@@ -36,7 +34,6 @@ class BlogPostsController < ApplicationController
   end
 
   def destroy
-    @blog_post = BlogPost.find(params[:id])
     @blog_post.destroy
 
     redirect_to blog_posts_path
@@ -46,5 +43,9 @@ class BlogPostsController < ApplicationController
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :description, :body, :image, :published)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
   end
 end
