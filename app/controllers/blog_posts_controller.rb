23a3eb0c6921358083c22a29057c7fe71970515c
leo-narra <141992134+leo-narra@users.noreply.github.com>
2024-07-21 22:04:16 +0800

    Added show action template

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,4 +2,8 @@ class BlogPostsController < ApplicationController
   def index
     @blog_posts = BlogPost.published.latest
   end
+
+  def show
+    @blog_post = BlogPost.find(params[:id])
+  end
 end
