9aa12b35f631fd2cc2502223b70003103e25a10a
leo-narra <141992134+leo-narra@users.noreply.github.com>
2024-07-21 22:38:48 +0800

    Added destroy action ui

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -35,6 +35,13 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def destroy
+    @blog_post = BlogPost.find(params[:id])
+    @blog_post.destroy
+
+    redirect_to blog_posts_path
+  end
+
   private
 
   def blog_post_params
